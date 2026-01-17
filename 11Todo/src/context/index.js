6e44e99989fe431f22b8a943d6cddb56953e9331
Niraj Karnawat <karnawatniraj@gmail.com>
2026-01-17 19:23:38 +0530

    new project

---
+++ 11Todo/src/context/index.js
@@ -0,0 +1,3 @@
+//we use this index.js file to re-export the context and provider for easier imports in other parts of the application.
+
+import {TodoContext,useContext,useTodo} from "./TodoContext";
